68125ac2734c83953af7ca884e8ddd2c829d5439
zhanghongsheng000 <2417112386@qq.com>
2019-05-19 13:08:05 +0800

    提交更改

---
+++ static/url.js
@@ -0,0 +1,6 @@
+/*测试地址*/
+let gPasserUrl = 'http://192.168.2.5:11000/'//外网通行客服务器地址
+let gUrl = 'http://192.168.2.5:10000/' //外网代发工资，物业缴费服务器地址
+let gDownLoad = 'http://192.168.2.5/download/file/' //模板下载地址
+let gAccountUrl = 'http://192.168.2.5:8888/'//外网财务地址
+let gImgUrl = '' //外网图片地址
